f60b8760a32efcfe449e678daac1fc5717f892e2
Rafal Walkow <r.walkow@po.edu.pl>
2022-08-17 16:13:54 +0200

    Modify components

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -1,19 +1,26 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, Progress, Alert } from 'reactstrap';
-import { getSeats, getRequests, loadSeatsRequest } from '../../../redux/seatsRedux';
+import { getSeats, getRequests, loadSeatsRequest, loadSeats } from '../../../redux/seatsRedux';
 import './SeatChooser.scss';
 import io from 'socket.io-client';
 
 const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
+  const allSeats = 50;
   const dispatch = useDispatch();
   const seats = useSelector(getSeats);
   const requests = useSelector(getRequests);
+ // eslint-disable-next-line no-unused-vars
   const [socket, setSocket] = useState();
 
   useEffect(() => {
     const SERVER_URL = (process.env.NODE_ENV === 'production') ? '' : 'http://localhost:8000';
     const socket = io(SERVER_URL);
+
+    socket.on('seatsUpdated', (seats) => {
+      dispatch(loadSeats(seats));
+    });
+
     setSocket(socket);
     return () => socket.close();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -22,6 +29,13 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
   useEffect(() => {
     dispatch(loadSeatsRequest());
   }, [dispatch])
+
+  const countNoFreeSeats = () => {
+    const noFreeSeats = seats.filter((seat) => seat.day === chosenDay);
+    if (noFreeSeats) {
+      return noFreeSeats.length;
+    }
+  }
 
   const isTaken = (seatId) => {
     return (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
@@ -38,11 +52,13 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
       <h3>Pick a seat</h3>
       <small id="pickHelp" className="form-text text-muted ml-2"><Button color="secondary" /> – seat is already taken</small>
       <small id="pickHelpTwo" className="form-text text-muted ml-2 mb-4"><Button outline color="primary" /> – it's empty</small>
-      { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
-      { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
+      { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(allSeats)].map((x, i) => prepareSeat(i+1) )}</div>}
+      { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={allSeats} /> }
       { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
+      <p>Free seats: {countNoFreeSeats()}/{allSeats}</p>
     </div>
   )
 }
 
 export default SeatChooser;
+
